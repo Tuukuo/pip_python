6636f4d92e0f05f4d220a7e4aa2dfb138ea53e17
Tuukuo <tuukuodorcas@gmail.com>
2024-02-28 21:02:11 +0300

    add files

--- assignment.py
+++ assignment.py
@@ -1,39 +1,21 @@
-def add  (x,y):
-    #This function is used for adding two numbers
-    return x + y
+#subtraction
+x=100
+y=25
+x-y
+result=x-y
+#addition
+x=100
+y=25
+x+y
+result=x+y
 
-def subtract (x,y):
-    #This function is used for subtracting two numbers
-    return x - y
-
-def multiply (x,y):
-    #This function is used for multiplying two numbers
-    return x * y
-
-def divide (x,y):
-    #This function is used for dividing two numbers
-    return (x / y)
-#Take input from the user
-print("please select the operation")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-choice=input("please enter choice 1/2/3/4:")
-num_1=int(input("please enter the first number:"))
-num_2=int(input("please enter the second number:"))
-
-if choice== '1':
-    print(num_1,"+",num_2,"=",add(num_1,num_2))
-
-elif choice=='2':
-    print(num_1,"-",num_2,"=",subtract(num_1,num_2))
-
-elif choice=='3':
-    print(num_1,"*",num_2,"=",multiply(num_1,num_2))
-
-elif choice=='4':
-    print(num_1,"/",num_2,"=",divide(num_1,num_2))
-
-    
-
+#multiplication
+x=100
+y=25
+x*y
+result=x*y
+#division
+x=100
+y=25
+x/y
+result=x/y
